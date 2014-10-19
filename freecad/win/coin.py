e14acb12e9086bb263684b618258a1be9c262fae
Peter Lama <peterldev94@gmail.com>
2014-10-18 19:52:43 -0700

    coin: upgrade to 4.0.0a (HEAD)

--- freecad/win/coin.py
+++ freecad/win/coin.py
@@ -1,38 +1,44 @@
 from clbundler.formula import *
         
 class coin(Formula):
-    version = "3.1.3"
+    version = "4.0.0a"
     source = {
-        "type":"archive", 
-        "url":"https://bitbucket.org/Coin3D/coin/downloads/Coin-{0}.tar.gz".format(version)
+        "type":"hg", 
+        "url":"https://bitbucket.org/Coin3D/coin"
     }
     supported = {"vc9":["x86", "x64"], "vc12":["x86", "x64"]}
     
     def __init__(self, context, options={}):
         super(coin, self).__init__(context, options)
         
-        self.patches = ["vcproj_x64", "macro_error"]
+        self.patches = ["vcproj_x64"]
         if context.toolchain == "vc12":
             self.patches.append("config")
        
     def build(self):
-        os.chdir("build\\msvc9")
-        
-        vcproj = "coin3.vcproj"
-        if self.context.toolchain == "vc12":
-            vcproj = vcproj_upgrade(vcproj)
+        vcproj = ""
+        extra = []
+        vc_ver = int(vc_version(self.context.toolchain))
+        if vc_ver == 9:
+            os.chdir("build\\msvc9")
+            vcproj = "coin4.vcproj"
+        elif vc_ver >= 10:
+            os.chdir("build\\msvc10")
+            vcproj = "coin4.vcxproj"
+            if vc_ver != 10:
+                extra = ["/tv:{0}.0".format(vc_ver), "/p:PlatformToolset=v{0}0".format(vc_ver)]
         
         if "debug" in self.variant:
-            vcbuild(self.context, vcproj, "DLL (Debug)")
+            vcbuild(self.context, vcproj, "DLL (Debug)", extra=extra)
         if "release" in self.variant:
-            vcbuild(self.context, vcproj, "DLL (Release)")
+            vcbuild(self.context, vcproj, "DLL (Release)", extra=extra)
         
         self.context.env["COINDIR"] = self.context.install_dir
         
         if "debug" in self.variant:
-            system.run_cmd("..\\misc\\install-sdk.bat", ["dll", "debug", "msvc9", "coin3"])
+            system.run_cmd("..\\misc\\install-sdk.bat", ["dll", "debug", "msvc9", "coin4"])
         if "release" in self.variant:
-            system.run_cmd("..\\misc\\install-sdk.bat", ["dll", "release", "msvc9", "coin3"])
+            system.run_cmd("..\\misc\\install-sdk.bat", ["dll", "release", "msvc9", "coin4"])
         
         os.chdir(self.context.install_dir)
         
